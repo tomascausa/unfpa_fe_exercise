93e9d1a2f8f5067aa069a4b81ae96c20a1ac8c60
Tomas Causa <tomas.causa@cloudgaia.com>
2021-04-28 06:10:04 -0300

    Using GSAP pro

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin';
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(DrawSVGPlugin);
